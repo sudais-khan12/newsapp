81ae69de006dbfe362cd411458eccd4d3add47a7
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2024-07-09 20:34:44 +0500

    changed

--- src/components/News.js
+++ src/components/News.js
@@ -6,8 +6,8 @@ export class News extends Component {
     return (
       <>
         <div className="container my-3">
-          <h2>Top Headings</h2>
-          <div className="row">
+          <h2 className="text-center my-4">Top Headings</h2>
+          <div className="container row">
             <div className="col-md-4">
               <NewsItem
                 title="Sample Title"
