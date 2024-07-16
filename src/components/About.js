68bc2eb0a72ff275151aaa084e0d2188de8b764f
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2024-07-16 20:59:48 +0500

    changed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class About extends Component {
   render() {
-    return <> This app is about News. And it is build with React </>;
+    return <> </>;
   }
 }
 
